15c34545d7c811c1f52a5179b1ff9b9f5e5c5cb0
Alexander <makedonski_rulez@mail.ru>
2024-11-06 00:54:59 +0300

    dops3_finish

--- src/App.tsx
+++ src/App.tsx
@@ -34,23 +34,23 @@ function App() {
     // убираем заглушки в типизации и вставляем в качестве инициализационного значения defaultMoney
     const [money, setMoney] = useState<MoneyType[]>(defaultMoney)
     const [filterValue, setFilterValue] = useState<BanknotsType>('ALL')   // по умолчанию указываем все банкноты
-    console.log(filterValue)
 
     // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
     // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
     const filteredMoney = moneyFilter(money, filterValue)
-    console.log(filteredMoney)
 
     const addMoney = (banknote: BanknotsType) => {
+        const newMoney:MoneyType = {banknote, nominal: 100, id: v1()}
+        setMoney([...money, newMoney])
         // Добавление денег сделаем в последнюю очередь, после настройки фильтров и отрисовки денег
     }
 
     const removeMoney = (banknote: BanknotsType) => {
         // Снятие денег сделаем в последнюю очередь, после настройки фильтров и отрисовки денег
-       // const index = money.findIndex
-       //  if (index !== -1) {
-       //      setMoney(money.filter((el, i) => ...));
-       //  }
+       const index = money.findIndex(el => el.banknote === banknote)
+        if (index !== -1) {
+            setMoney(money.filter((el, i) => i !== index));
+        }
     }
 
     return (
@@ -58,6 +58,8 @@ function App() {
             <Country
                 data={filteredMoney}   //отрисовать будем деньги после фильтрации
                 setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
+                addMoney={addMoney}
+                removeMoney={removeMoney}
             />
         </div>
     );
