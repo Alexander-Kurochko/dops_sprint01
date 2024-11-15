b83707cb3cc6102e225834e6d47ca31e0179b5cd
Alexander <makedonski_rulez@mail.ru>
2024-11-15 21:54:23 +0300

    dops_sprint02_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,134 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Country} from "./Country";
-import {v1} from "uuid";
+import {TaskType, Todolist} from './Todolist';
+import {v1} from 'uuid';
 
-export type BanknotsType = 'USD' | 'RUB' | 'ALL' // создадим типы для banknotes -он может быть 'DOLLARS', 'RUBLS' или 'All'
-export type MoneyType = {
-    banknote: BanknotsType
-    nominal: number// не ленимся, убираем заглушку, и пишем правильный тип)
-    id: string// ложку за Димыча, за...
+export type FilterValuesType = 'all' | 'active' | 'completed';
+type TodolistsType = { id: string, title: string }
+type TasksType = {
+    [key: string]: SuperTaskType
 }
 
-let defaultMoney: MoneyType[] = [  // типизируем
-    {banknote: 'USD', nominal: 100, id: v1()},
-    {banknote: 'USD', nominal: 100, id: v1()},
-    {banknote: 'RUB', nominal: 100, id: v1()},
-    {banknote: 'USD', nominal: 100, id: v1()},
-    {banknote: 'USD', nominal: 100, id: v1()},
-    {banknote: 'RUB', nominal: 100, id: v1()},
-    {banknote: 'USD', nominal: 100, id: v1()},
-    {banknote: 'RUB', nominal: 100, id: v1()},
-]
-
-
-export const moneyFilter = (money: MoneyType[], filter: BanknotsType): MoneyType[] => {
-    if (filter === 'ALL') return money
-    return money.filter(el => el.banknote === filter)
-    //если пришел filter со значением 'All', то возвращаем все банкноты
-    //return money.filter... ну да, придется фильтровать
+type SuperTaskType = {
+    data: TaskType[],
+    filter: FilterValuesType
 }
-
 
 function App() {
-    // убираем заглушки в типизации и вставляем в качестве инициализационного значения defaultMoney
-    const [money, setMoney] = useState<MoneyType[]>(defaultMoney)
-    const [filterValue, setFilterValue] = useState<BanknotsType>('ALL')   // по умолчанию указываем все банкноты
+    // let todolistID1 = v1();
+    // let todolistID2 = v1();
+    //
+    // let [todolists, setTodolists] = useState<Array<TodolistsType>>([
+    //     {id: todolistID1, title: 'What to learn', filter: 'all'}, //0
+    //      {id: todolistID2, title: 'What to buy', filter: 'all'},  //1
+    // ])
+    //
+    // let [tasks, setTasks] = useState({
+    //     [todolistID1]: [
+    //         {id: v1(), title: "HTML&CSS", isDone: true},
+    //         {id: v1(), title: "JS", isDone: true},
+    //         {id: v1(), title: "ReactJS", isDone: false},
+    //         {id: v1(), title: "Rest API", isDone: false},
+    //         {id: v1(), title: "GraphQL", isDone: false},
+    //     ],
+    //     [todolistID2]: [
+    //         {id: v1(), title: "HTML&CSS2", isDone: true},
+    //         {id: v1(), title: "JS2", isDone: true},
+    //         {id: v1(), title: "ReactJS2", isDone: false},
+    //         {id: v1(), title: "Rest API2", isDone: false},
+    //         {id: v1(), title: "GraphQL2", isDone: false},
+    //     ]
+    // });
 
-    // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
-    // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
-    const filteredMoney = moneyFilter(money, filterValue)
+    let todolistId1 = v1();
+    let todolistId2 = v1();
 
-    const addMoney = (banknote: BanknotsType) => {
-        const newMoney:MoneyType = {banknote, nominal: 100, id: v1()}
-        setMoney([...money, newMoney])
-        // Добавление денег сделаем в последнюю очередь, после настройки фильтров и отрисовки денег
+    let [todolists, setTodolists] = useState<TodolistsType[]>([
+        {id: todolistId1, title: 'What to learn'},
+        {id: todolistId2, title: 'What to buy'}
+    ])
+
+    let [tasks, setTasks] = useState<TasksType>({
+        [todolistId1]: {
+            data: [
+                {id: v1(), title: 'HTML&CSS1111', isDone: true},
+                {id: v1(), title: 'JS1111', isDone: true}
+            ],
+            filter: 'all'
+        },
+        [todolistId2]: {
+            data: [
+                {id: v1(), title: 'HTML&CSS22222', isDone: true},
+                {id: v1(), title: 'JS2222', isDone: true}
+            ],
+            filter: 'all'
+        }
+    });
+
+
+    const removeTodolist = (todolistId: string) => {
+        setTodolists(todolists.filter(el => el.id !== todolistId))
+        delete tasks[todolistId]
+        console.log(tasks)
     }
 
-    const removeMoney = (banknote: BanknotsType) => {
-        // Снятие денег сделаем в последнюю очередь, после настройки фильтров и отрисовки денег
-       const index = money.findIndex(el => el.banknote === banknote)
-        if (index !== -1) {
-            setMoney(money.filter((el, i) => i !== index));
-        }
+
+    function removeTask(todolistId: string, taskId: string) {
+        setTasks((prevState) => ({...prevState,
+            [todolistId]: {...prevState[todolistId], data: prevState[todolistId].data.filter(el => el.id !== taskId)}}))
+    }
+
+    function addTask(todolistId: string, title: string) {
+        let newTask = {id: v1(), title: title, isDone: false};
+        setTasks((prevState) => ({
+            ...prevState,
+            [todolistId]: {...prevState[todolistId], data : [...prevState[todolistId].data, newTask ]}
+        }))
+        // let newTasks = [task, ...tasks];
+        // setTasks(newTasks);
+    }
+
+    function changeStatus(todolistId: string, taskId: string, newIsDone: boolean) {
+        setTasks((prevState) => ({
+            ...prevState,
+            [todolistId]: {...prevState[todolistId], data: prevState[todolistId].data.map(el => el.id === taskId ? {...el, isDone: newIsDone} : el)}
+        }))
+    }
+
+    function changeFilter(todolistId: string, value: FilterValuesType) {
+        // setTodolists(todolists.map(el => el.id === todolistId ? {...el, filter: value} : el))
+        setTasks((prevState) => ({
+            ...prevState,
+            [todolistId] : {...prevState[todolistId], filter: value}}))
     }
 
     return (
         <div className="App">
-            <Country
-                data={filteredMoney}   //отрисовать будем деньги после фильтрации
-                setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
-                addMoney={addMoney}
-                removeMoney={removeMoney}
-            />
+            {todolists.map((el) => {
+                let tasksForTodolist = tasks[el.id].data;
+                // if (tasks[el.id].filter === 'active') {
+                //     tasksForTodolist = tasks[el.id].data.filter(t => t.isDone === false);
+                // }
+                // if (tasks[el.id].filter === 'completed') {
+                //     tasksForTodolist = tasks[el.id].data.filter(t => t.isDone === true);
+                // }
+                return (
+                    <Todolist
+                        key={el.id}
+                        todolistId={el.id}
+                        title={el.title}
+                        tasks={tasksForTodolist}
+                        removeTask={removeTask}
+                        changeFilter={changeFilter}
+                        addTask={addTask}
+                        changeTaskStatus={changeStatus}
+                        filter={tasks[el.id].filter}
+                        removeTodolist={removeTodolist}
+                    />
+                )
+            })}
+
+
         </div>
     );
 }
